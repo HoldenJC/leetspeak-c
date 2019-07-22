029547837b62f84e6eabf7dea3ed3664f41f0ad5
HoldenJC <holdenj_c@yahoo.com>
2019-07-22 12:10:57 -0700

    add code for functioning Leetspeak translator and all MSTests used to test program

--- LeetSpeak.Tests/ModelTests/LeetspeakTranslationTests.cs
+++ LeetSpeak.Tests/ModelTests/LeetspeakTranslationTests.cs
@@ -12,6 +12,39 @@ namespace Leetspeak.Tests
     {
       Leetspeaks testLeetspeak = new Leetspeaks();
       Assert.AreEqual("3", testLeetspeak.LeetspeakTranslation("e"));
+      Assert.AreEqual("3", testLeetspeak.LeetspeakTranslation("E"));
+    }
+
+    [TestMethod]
+    public void LeetspeakTranslation_TranslationOfO_0()
+    {
+      Leetspeaks testLeetspeak = new Leetspeaks();
+      Assert.AreEqual("0", testLeetspeak.LeetspeakTranslation("o"));
+      Assert.AreEqual("0", testLeetspeak.LeetspeakTranslation("O"));
+    }
+
+    [TestMethod]
+    public void LeetspeakTranslation_TranslationOfT_7()
+    {
+      Leetspeaks testLeetspeak = new Leetspeaks();
+      Assert.AreEqual("7", testLeetspeak.LeetspeakTranslation("t"));
+      Assert.AreEqual("7", testLeetspeak.LeetspeakTranslation("T"));
+    }
+
+    [TestMethod]
+    public void LeetspeakTranslation_TranslationOfI_1()
+    {
+      Leetspeaks testLeetspeak = new Leetspeaks();
+      Assert.AreEqual("1", testLeetspeak.LeetspeakTranslation("I"));
+      Assert.AreEqual("i", testLeetspeak.LeetspeakTranslation("i"));
+    }
+
+    [TestMethod]
+    public void LeetspeakTranslation_TranslationOfS_z()
+    {
+      Leetspeaks testLeetspeak = new Leetspeaks();
+      Assert.AreEqual("sz", testLeetspeak.LeetspeakTranslation("ss"));
+      Assert.AreEqual("sz shz", testLeetspeak.LeetspeakTranslation("ss shS"));
     }
 
 
--- Leetspeak/Models/LeetspeakTranslation.cs
+++ Leetspeak/Models/LeetspeakTranslation.cs
@@ -15,13 +15,41 @@ namespace Leetspeak
 
     public string LeetspeakTranslation(string text)
     {
-      string resultString = "";
+      char[] wordOriginal = text.ToCharArray();
+      char[] wordLeet = new char[wordOriginal.Length];
 
-      return resultString;
+      for(int i = 0; i < text.Length; i++)
+      {
+        if(wordOriginal[i] == 'e' || wordOriginal[i] == 'E')
+        {
+          wordLeet[i] = '3';
+        } else if(wordOriginal[i] == 'o' || wordOriginal[i] == 'O')
+        {
+          wordLeet[i] = '0';
+        } else if(wordOriginal[i] == 't' || wordOriginal[i] == 'T')
+        {
+          wordLeet[i] = '7';
+        } else if(wordOriginal[i] == 'I')
+        {
+          wordLeet[i] = '1';
+        } else if((wordOriginal[i] == 's' || wordOriginal[i] == 'S') && i != 0 && wordOriginal[i-1] != ' ')
+        {
+          wordLeet[i] = 'z';
+        } else
+        {
+          wordLeet[i] = wordOriginal[i];
+        }
+      }
+      string result = string.Join("", wordLeet);
+      return result;
     }
   }
 }
 
-//                      PLAIN ENGLISH SPECS
-//-------------------------------------------------------------------
-//
+//                                     PLAIN ENGLISH SPECS
+//------------------------------------------------------------------------------------------------
+//current character is 'e' or 'E' -------------------------------------> program replaces with "3"
+//current character is 'o' or 'O' -------------------------------------> program replaces with "0"
+//current character is 't' or 'T' -------------------------------------> program replaces with "7"
+//current character is 'I' --------------------------------------------> program replaces with "1"
+//current character is 's' or 'S' and not first letter of word --------> program replaces with "z"
